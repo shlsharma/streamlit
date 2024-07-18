d999af9c918620a3c4fbf7aaaee55057122d2146
shlsharma <shlsharma.sharma@gmail.com>
2024-07-18 09:33:49 +0530

    v3

--- app.py
+++ app.py
@@ -1,8 +1,57 @@
 import joblib
 import streamlit as st
+import numpy as np
 
 model_name = 'RF_Loan_model.joblib'
 model = joblib.load(model_name)
+
+def prediction(Gender, Married, Dependents, Education, SelfEmployed,
+                ApplicantIncome, CoapplicantIncome, LoanAmount, LoanAmountTerm, CreditHistory,
+                PropertyArea):
+    if Gender == "Male":
+        Gender = 1
+    else:
+        Gender = 0
+
+    if Married == "Yes":
+        Married = 1
+    else:
+        Married = 0
+
+    if Education == "Graduate":
+        Education = 0
+    else:
+        Education = 1
+
+    if SelfEmployed == "Yes":
+        SelfEmployed = 1
+    else:
+        SelfEmployed = 0
+
+    TotalIncome = np.log(ApplicantIncome+CoapplicantIncome)
+
+    if CreditHistory == "Outstanding Loan":
+        CreditHistory = 1
+    else:
+        CreditHistory = 0
+
+    if PropertyArea == "Rural":
+        PropertyArea = 0
+    elif PropertyArea == "Semi Urban":
+        PropertyArea = 1
+    else:
+        PropertyArea = 2
+
+    predict = model.predict([[Gender, Married, Dependents, Education, SelfEmployed, LoanAmount, 
+                             LoanAmountTerm, CreditHistory, PropertyArea, TotalIncome]])
+    print(print(predict))
+
+    if predict == 0:
+        pred = "Rejected"
+    else:
+        pred = "Approved"
+    
+    return pred
 
 def main():
     # front end
@@ -21,7 +70,15 @@ def main():
     CreditHistory = st.selectbox('Credit History', ('Outstanding Loan', 'No Outstanding Loan'))
     PropertyArea = st.selectbox('Property Area', ('Rural', 'Urban', 'Semi Urban'))
 
-    if st.button('Submit')
+    if st.button('Predict'):
+        result = prediction(Gender, Married, Dependents, Education, SelfEmployed,
+                            ApplicantIncome, CoapplicantIncome, LoanAmount, LoanAmountTerm, CreditHistory,
+                            PropertyArea)
+        
+        if result == "Approved":
+            st.success("Your Loan Application is Approved")
+        else:
+            st.error("Your Loan Application is Rejected")
 
 if __name__ == '__main__':
     main()
