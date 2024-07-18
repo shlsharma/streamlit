24dd796c38a763155b5f7ce5b85eae93d8c3a8fc
Sahil Sharma <47265746+shlsharma@users.noreply.github.com>
2024-07-18 06:00:04 +0000

    Added file url

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import joblib
 import streamlit as st
 import numpy as np
+import wget
 
 model_name = 'RF_Loan_model.joblib'
 file_url = "https://raw.githubusercontent.com/manifoldailearning/Complete-MLOps-BootCamp/main/Build-ML-App-Streamlit/RF_Loan_model.joblib"
